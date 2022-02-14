edd50b890bce28b15d4e3ba3c59ac14303ca2921
Noah Biro <noah.biro@vivun.com>
2022-02-14 12:05:32 -0500

    Hello Kitty

--- src/App.js
+++ src/App.js
@@ -1,9 +1,75 @@
 import './App.css';
 
-const hello_world = <h1>Hello, World!</h1>;
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+    return (
+        <img className="Avatar"
+             src={props.user.avatarUrl}
+             alt={props.user.name}/>
+    );
+}
+
+function UserInfo(props) {
+    return (
+        <div className="UserInfo">
+            <Avatar user={props.user}/>
+            <div className="UserInfo-name">
+                {props.user.name}
+            </div>
+        </div>
+    );
+}
+
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <UserInfo user={props.author}/>
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
+    );
+}
+
+function Greeting(props) {
+    return (
+        <Comment
+            date={props.date}
+            text={props.text}
+            author={props.author}
+        />
+    )
+}
+
+// function Greeting({date, text, author}) {
+//     return (
+//         <Comment
+//             date={date}
+//             text={text}
+//             author={author}
+//         />
+//     )
+// }
+
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'http://placekitten.com/g/64/64'
+    }
+};
+
 
 function App() {
-  return hello_world
+    return Greeting(comment)
 }
 
 export default App;
