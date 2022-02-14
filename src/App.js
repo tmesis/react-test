cd721e66fab7345a919067be07f0206cb202bc6b
Noah Biro <noah.biro@vivun.com>
2022-02-14 15:39:27 -0500

    Controlled Text Example

--- src/App.js
+++ src/App.js
@@ -2,27 +2,37 @@ import './App.css';
 import React from "react";
 
 
-const numbers = [1, 2, 3, 4, 5];
+class NameForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+    }
 
+    handleChange = (event) => {
+        this.setState({value: event.target.value});
+    }
 
-function ListItem({value}) {
-    return <li>{value}</li>;
-}
+    handleSubmit = (event) => {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
 
-function NumberList({numbers}) {
-    return (
-        <ul>
-            {numbers.map((number) =>
-                <ListItem key={number.toString()}
-                          value={number}/>
-            )}
-        </ul>
-    );
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange}/>
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+        );
+    }
 }
 
 
 function App() {
-    return <NumberList numbers={numbers}/>
+    return <NameForm/>
 }
 
 export default App;
