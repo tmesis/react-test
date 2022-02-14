d481fa92461172727acf3f1eaab8410e47a16cb4
Noah Biro <noah.biro@vivun.com>
2022-02-14 15:26:42 -0500

    Embedding map() in JSX

--- src/App.js
+++ src/App.js
@@ -2,40 +2,27 @@ import './App.css';
 import React from "react";
 
 
-const posts = [
-    {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
-    {id: 2, title: 'Installation', content: 'You can install React from npm.'}
-];
+const numbers = [1, 2, 3, 4, 5];
 
 
-function Blog({posts}) {
-    const sidebar = (
+function ListItem({value}) {
+    return <li>{value}</li>;
+}
+
+function NumberList({numbers}) {
+    return (
         <ul>
-            {posts.map((post) =>
-                <li key={post.id}>
-                    {post.title}
-                </li>
+            {numbers.map((number) =>
+                <ListItem key={number.toString()}
+                          value={number}/>
             )}
         </ul>
-    );
-    const content = posts.map((post) =>
-        <div key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content}</p>
-        </div>
-    );
-    return (
-        <div>
-            {sidebar}
-            <hr/>
-            {content}
-        </div>
     );
 }
 
 
 function App() {
-    return <Blog posts={posts}/>
+    return <NumberList numbers={numbers}/>
 }
 
 export default App;
