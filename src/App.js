66eb32ce8639bbef12265de4ca135fea5b3b1a04
Noah Biro <noah.biro@vivun.com>
2022-02-14 16:35:49 -0500

    Untitled

--- src/App.js
+++ src/App.js
@@ -2,95 +2,30 @@ import './App.css';
 import React from "react";
 
 
-const scaleNames = {
-    c: 'Celsius',
-    f: 'Fahrenheit'
-};
-
-
-function toCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5 / 9;
+function FancyBorder({color, children}) {
+    return (
+        <div className={'FancyBorder FancyBorder-' + color}>
+            {children}
+        </div>
+    );
 }
 
-function toFahrenheit(celsius) {
-    return (celsius * 9 / 5) + 32;
-}
-
-function tryConvert(temperature, convert) {
-    const input = parseFloat(temperature);
-    if (Number.isNaN(input)) {
-        return '';
-    }
-    const output = convert(input);
-    const rounded = Math.round(output * 1000) / 1000;
-    return rounded.toString();
-}
-
-function BoilingVerdict(props) {
-    if (props.celsius >= 100) {
-        return <p>The water would boil.</p>;
-    }
-    return <p>The water would not boil.</p>;
-}
-
-class TemperatureInput extends React.Component {
-    handleChange = (event) => {
-        this.props.onTemperatureChange(event.target.value);
-    }
-
-    render() {
-        const temperature = this.props.temperature;
-        const scale = this.props.scale;
-        return (
-            <fieldset>
-                <legend>Enter temperature in {scaleNames[scale]}:</legend>
-                <input value={temperature}
-                       onChange={this.handleChange}/>
-            </fieldset>
-        );
-    }
-}
-
-class Calculator extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {temperature: '', scale: 'c'};
-    }
-
-    handleCelsiusChange = (temperature) => {
-        this.setState({scale: 'c', temperature: temperature});
-    }
-
-    handleFahrenheitChange = (temperature) => {
-        this.setState({scale: 'f', temperature: temperature});
-    }
-
-    render() {
-        const scale = this.state.scale;
-        const temperature = this.state.temperature;
-        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
-        const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
-
-        return (
-            <div>
-                <TemperatureInput
-                    scale="c"
-                    temperature={celsius}
-                    onTemperatureChange={this.handleCelsiusChange}/>
-                <TemperatureInput
-                    scale="f"
-                    temperature={fahrenheit}
-                    onTemperatureChange={this.handleFahrenheitChange}/>
-                <BoilingVerdict
-                    celsius={parseFloat(celsius)}/>
-            </div>
-        );
-    }
+function WelcomeDialog() {
+    return (
+        <FancyBorder color="blue">
+            <h1 className="Dialog-title">
+                Welcome
+            </h1>
+            <p className="Dialog-message">
+                Thank you for visiting our spacecraft!
+            </p>
+        </FancyBorder>
+    );
 }
 
 
 function App() {
-    return <Calculator/>
+    return <WelcomeDialog/>
 }
 
 export default App;
