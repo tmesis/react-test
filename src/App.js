cef6a4c1a52c0270128c39c70d35b629bf43f5d6
Noah Biro <noah.biro@vivun.com>
2022-02-14 16:47:20 -0500

    Specialization using classes

--- src/App.js
+++ src/App.js
@@ -10,27 +10,47 @@ function FancyBorder({color, children}) {
     );
 }
 
-function Dialog({title, message}) {
+function Dialog({title, message, children}) {
     return (
         <FancyBorder color="blue">
             <h1 className="Dialog-title">{title}</h1>
             <p className="Dialog-message">{message}</p>
+            {children}
         </FancyBorder>
     );
 }
 
-function WelcomeDialog() {
-    return (
-        <Dialog
-            title="Welcome"
-            message="Thank you for visiting our spacecraft!"
-        />
-    );
+class SignUpDialog extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {login: ''};
+    }
+
+    render() {
+        return (
+            <Dialog title="Mars Exploration Program" message="How should we refer to you?">
+                <input value={this.state.login}
+                       onChange={this.handleChange}
+                />
+                <button onClick={this.handleSignUp}>
+                    Sign Me Up!
+                </button>
+            </Dialog>
+        );
+    }
+
+    handleChange = (event) => {
+        this.setState({login: event.target.value});
+    }
+
+    handleSignUp = () => {
+        alert(`Welcome aboard, ${this.state.login}!`);
+    }
 }
 
 
 function App() {
-    return <WelcomeDialog/>
+    return <SignUpDialog/>
 }
 
 export default App;
